3bbd3bd7ab00c59a38c3ed0f6e709efe02c6bf7e
Cameron Arshadi <arshadic@hhmi.org>
2020-12-04 00:17:59 -0500

    Add files and update code for MouseLight population analysis

--- FigS7_PopulationDiagrams/multigraph_pt_neurons.py
+++ FigS7_PopulationDiagrams/multigraph_pt_neurons.py
@@ -1,9 +1,9 @@
-#@Context context
+# @Context context
 
+import os
+import re
 from sc.fiji.snt import Tree
-from sc.fiji.snt.analysis import *
-from sc.fiji.snt.analysis.graph import AnnotationGraph, AnnotationWeightedEdge, GraphColorMapper, SNTPseudograph
-from sc.fiji.snt.annotation import AllenCompartment
+from sc.fiji.snt.analysis.graph import AnnotationGraph, GraphColorMapper
 from sc.fiji.snt.io import MouseLightLoader
 from sc.fiji.snt.viewer import GraphViewer
 from sc.fiji.snt.viewer.geditor import mxCircleLayoutGrouped
@@ -13,76 +13,12 @@ from sc.fiji.snt.viewer.geditor import mxCircleLayoutGrouped
 # Latest SNT API: https://morphonets.github.io/SNT/
 
 
-annotation_pool = {} # global
-
-        
-def build_edge_dict(g):
-    edge_dict = {}
-    for e in g.edgeSet():
-        e_string = str(e.getSource().id()) + "," + str(e.getTarget().id())
-        edge_dict[e_string] = e
-    return edge_dict
-
-
-def merge_graphs(g1, g2):
-    edge_dict_1 = build_edge_dict(g1)
-    edge_dict_2 = build_edge_dict(g2)
-    intersection_keys = set(edge_dict_1.keys()) & set(edge_dict_2.keys())
-    merged_graph = SNTPseudograph(AnnotationWeightedEdge)
-    for edge_str in intersection_keys:
-        edge_str_split = edge_str.split(',')
-        source = get_node(int(edge_str_split[0]))
-        target = get_node(int(edge_str_split[1]))
-        if not merged_graph.containsVertex(source):
-            merged_graph.addVertex(source)
-        if not merged_graph.containsVertex(target):
-            merged_graph.addVertex(target)
-            
-        new_edge = merged_graph.addEdge(source, target)
-        merged_graph.setEdgeWeight(new_edge, edge_dict_1[edge_str].getWeight())
-        # Use edge color from source graph
-        merged_graph.setEdgeColor(new_edge, g1.getEdgeColor(edge_dict_1[edge_str]))
-        
-        new_edge = merged_graph.addEdge(source, target)
-        merged_graph.setEdgeWeight(new_edge, edge_dict_2[edge_str].getWeight())
-        # Use edge color from source graph
-        merged_graph.setEdgeColor(new_edge, g2.getEdgeColor(edge_dict_2[edge_str]))
-        
-    return merged_graph
-
-
-def get_diagonal(compartment):
-    diag = diagonal_dict.get(compartment)
-    if diag is None:
-        mesh = compartment.getMesh()
-        if mesh is None:
-            return None
-        diag = mesh.getBoundingBox('left').getDiagonal()
-        diagonal_dict[compartment] = diag
-    return diag
-
-def get_node(int_id):
-    global annotation_pool
-    node = annotation_pool.get(int_id)
-    if node is None:
-        node = AllenCompartment(int_id)
-        annotation_pool[int_id] = node
-    return node
-
-def get_trees(id_list):
-    trees = []
-    for i in id_list:
-        t = MouseLightLoader(i).getTree("axon")
-        trees.append(t)
-    return trees
-
 def show_graph(graph):
-
     # Color code edges (i.e., run Graph Viewer's Analyze>Color Coding... commmand)
     mapper = GraphColorMapper(context)
-    mapper.setMinMax(10, 400) # 10-400 Axonal endings
-    mapper.map(graph, GraphColorMapper.EDGE_WEIGHT, "mpl-viridis") 
- 
+    mapper.setMinMax(10, 400)  # 10-400 Axonal endings
+    mapper.map(graph, GraphColorMapper.EDGE_WEIGHT, "mpl-viridis")
+
     viewer = GraphViewer(graph)
     viewer.setContext(context)
     editor = viewer.getEditor()
@@ -95,6 +31,7 @@ def show_graph(graph):
     # command
     grouped_layout = mxCircleLayoutGrouped(snt_graph_adapter, 7, 4)
     grouped_layout.setRadius(400)
+    grouped_layout.setCenterSource(True)
     editor.applyLayout(grouped_layout)
 
     # Display result. NB: Aesthetic customizations (font size, label background,
@@ -102,24 +39,22 @@ def show_graph(graph):
     # (e.g., 'Diagram>Style>Minimal Style')
     viewer.show()
 
+
 def main():
     """
-    Speficy the two types of mouse neurons from MOs with differing projection patterns
+    Speficy three types of mouse motor neuron with differing projection patterns
     described in Winnubst et al. 2019 (https://pubmed.ncbi.nlm.nih.gov/31495573/):
-        med_ids:  Layer 5 Medulla projecting PT neurons 
-        thal_ids: Layer 6 Corticothalamic PT neurons
+    Layer 5 Medulla projecting PT neurons
+    Layer 5 Thalamic projecting PT neurons
+    Layer 6 Corticothalamic PT neurons
     """
-    med_ids = ['AA0011', 'AA0012', 'AA0115', 'AA0179', 'AA0180', 'AA0181', 'AA0182', 'AA0245', 
-              'AA0250', 'AA0576', 'AA0726', 'AA0788', 'AA0791', 'AA0792']
-    thal_ids = ['AA0039', 'AA0101', 'AA0103', 'AA0105', 'AA0188', 'AA0278', 'AA0390', 'AA0394', 
-              'AA0406', 'AA0577', 'AA0599', 'AA0633', 'AA0650', 'AA0781', 'AA0784', 'AA0799', 
-              'AA0817', 'AA0837', 'AA0838', 'AA0844']
-    thal_trees = get_trees(thal_ids)
-    med_trees = get_trees(med_ids)
-    thal_graph = AnnotationGraph(thal_trees, "tips", 10, 6)
-    med_graph = AnnotationGraph(med_trees, "tips", 10, 6)
-    show_graph(thal_graph)
-    show_graph(med_graph)
+    swc_dir = r"C:\Users\cam\Documents\repos\SNTmanuscript\FigS7_PopulationDiagrams\swc"
+    for group_dir in os.listdir(swc_dir):
+        group = []
+        for swc in os.listdir(os.path.join(swc_dir, group_dir)):
+            group.append(MouseLightLoader(re.sub('\.swc$', '', swc)).getTree('axon'))
+        graph = AnnotationGraph(group, "tips", 10, 6)
+        show_graph(graph)
 
 
 main()
